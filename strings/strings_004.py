dc7a8572e73cf523d085be3610a573b883c63827
selim <selim.mht@gmail.com>
2020-04-18 02:23:07 -0700

    Delete strings_004.py

--- strings/strings_004.py
+++
@@ -1,8 +0,0 @@
-#we can print different variables together with '+' sign
-
-first_name = "Selim"
-last_name = "Muhammet"
-
-print("Hi " + first_name)
-
-print("Dear Mr. " + first_name + " " + last_name)
