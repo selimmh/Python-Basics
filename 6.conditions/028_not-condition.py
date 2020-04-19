d458dd716e45c02cda723ba370ff4531a1643d6f
selim <selim.mht@gmail.com>
2020-04-19 12:52:22 +0300

    Add files via upload

---
+++ 6.conditions/028_not-condition.py
@@ -0,0 +1,12 @@
+# not condition
+
+# asking user to type password
+password = input("Enter password: ")
+
+# we check if password is identical or not
+# '!=' means not identical
+
+if password != 'selim123':
+    print("Incorrect password")
+else:
+    print("Logging in...")
