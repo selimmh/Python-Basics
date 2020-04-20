64072357a34567c552a0da011c766057f0e9ef6e
selim <selim.mht@gmail.com>
2020-04-20 21:56:18 +0300

    Add files via upload

---
+++ 7.dictionaries/033_intro-dictionaries.py
@@ -0,0 +1,8 @@
+# dictionaries
+
+# input the keyword and value
+cars = {'mercedes' : '10.000 $', 'tesla' : '22.000 $'}
+
+# print the value using keyword
+print("Price of the Mercedes is " + cars['mercedes'])
+print("Price of the Tesla is " + cars['tesla'])
