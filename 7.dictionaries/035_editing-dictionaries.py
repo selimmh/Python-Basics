67f0975ee9ec71e3fb0748af72f166cfe028707b
selim <selim.mht@gmail.com>
2020-04-22 00:17:19 -0700

    Delete 035_editing-dictionaries.py

--- 7.dictionaries/035_editing-dictionaries.py
+++
@@ -1,7 +0,0 @@
-# editing and deleting values in a dictionary
-
-books = {'author' : 'Khaled Hosseini', 'name' : 'Kite runner'}
-
-print(books.get('author'))
-print(books.get('name'))
-print(books.get('publisher', 'Not found.'))
