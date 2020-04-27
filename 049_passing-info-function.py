1fcb357b907068ba1292ad694f7c49bb60401910
selim <selim.mht@gmail.com>
2020-04-27 19:37:24 +0300

    Add files via upload

---
+++ 049_passing-info-function.py
@@ -0,0 +1,6 @@
+# passing information to a function
+
+def my_function(name):
+    print("Your name is " + name.title())
+
+my_function('selim')
---
+++ 054_return2.py
@@ -0,0 +1,6 @@
+# using return in functions v.2
+
+def my_function1(name,surname):
+    return name.title() + " " + surname.title()
+
+print(my_function1('selim', 'mh'))
